b2bc83d91cd74cd08f8927da91041ebfa7526668
Thisuraka <64680083+Thisuraka@users.noreply.github.com>
2020-08-24 21:22:19 +0530

    one

--- app/src/main/java/com/example/revisionlab/addReading.java
+++ app/src/main/java/com/example/revisionlab/addReading.java
@@ -1,7 +1,6 @@
 package com.example.revisionlab;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class addReading extends AppCompatActivity {
@@ -10,5 +9,16 @@ public class addReading extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reading);
+
+
+    }
+
+    private void changeFragment(){
+
+        String flag = getIntent().getStringExtra("number");
+
+        if(flag == "1"){
+
+        }
     }
 }
--- app/src/main/java/com/example/revisionlab/readingCollections.java
+++ app/src/main/java/com/example/revisionlab/readingCollections.java
@@ -2,7 +2,13 @@ package com.example.revisionlab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 public class readingCollections extends AppCompatActivity {
 
@@ -11,6 +17,27 @@ public class readingCollections extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reading_collections);
 
+        Button btn1 = (Button)findViewById(R.id.but3);
+        Button btn2 = (Button)findViewById(R.id.but4);
+        final String txt1 = "1";
+        final String txt2 = "2";
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(readingCollections.this, addReading.class);
+                intent.putExtra("number", txt1);
+                startActivity(intent);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(readingCollections.this, addReading.class);
+                intent.putExtra("number", txt2);
+                startActivity(intent);
+            }
+        });
     }
 }
